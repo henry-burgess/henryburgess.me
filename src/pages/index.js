5fe0dbd8cf1c0f42742e20cb4ff0296bb29230e5
Henry Burgess <henryjburg@gmail.com>
2021-01-17 09:09:15 +1000

    Add sass styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,57 +1,9 @@
+// Libraries
 import * as React from "react"
-import ProfileImage from '../assets/Profile.png'
 
-// Accent colour
-const accent = "#26DBBD"
-
-// Page styling
-const pageStyles = {
-  color: "#232129",
-  padding: "5vw 10vw 5vw 10vw",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 32,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: accent,
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-  listStyleType: "none",
-}
-const listItemStyles = {
-  marginBottom: 12,
-  fontWeight: "300",
-  letterSpacing: 1,
-}
-const linkStyles = {
-  color: "#1ba68f",
-}
-const profileStyles = {
-  borderRadius: "50%",
-  width: "10vw",
-  padding: "1vw 1vw 1vw 1vw",
-}
-const introDivStyles = {
-  backgroundColor: accent,
-  padding: "2vw 1vw 2vw 1vw",
-}
-const introHeadingStyles = {
-  marginTop: 20,
-  marginBottom: 32,
-  color: "#FFFFFF",
-}
-const introParagraphStyles = {
-  color: "#FFFFFF",
-  marginBottom: 20,
-}
+// Assets
+import ProfileImage from "../assets/Profile.png"
+import "../scss/main.scss"
 
 // Social Media links
 const links = [
@@ -75,34 +27,34 @@ const links = [
 
 const IndexPage = () => {
   return (
-    <main style={pageStyles}>
+    <main className={'pageStyles'}>
       <title>Henry Burgess</title>
 
-      <h1 style={headingStyles}>
+      <h1 className={'headingStyles'}>
         Hi!
         <br />
-        <span style={headingAccentStyles}>I'm Henry.</span>
+        <span className={'headingAccentStyles'}>I'm Henry.</span>
       </h1>
 
-      <div style={introDivStyles}>
-        <img src={ProfileImage} alt='Henry Burgess' style={profileStyles}></img>
-        <h2 style={introHeadingStyles}>
+      <div className={'introDivStyles'}>
+        <img src={ProfileImage} alt='Henry Burgess' className={'profileStyles'}></img>
+        <h2 className={'introHeadingStyles'}>
           I'm a Software Engineering student at the University of Queensland. 
         </h2>
-        <p style={introParagraphStyles}>
+        <p className={'introParagraphStyles'}>
           I have experience developing software that helps people stay safe, learn more about our body, and study the health of the land we live in!
         </p>
       </div>
 
       <br />
 
-      <h1 style={headingStyles}>
+      <h1 className={'headingStyles'}>
         Projects
       </h1>
-      <h2 style={headingStyles}>
+      <h2 className={'headingStyles'}>
         psygo
       </h2>
-      <p style={paragraphStyles}>
+      <p className={'paragraphStyles'}>
         psygo aims to be a tool that streamlines creating plugins for the <a href='https://www.jspsych.org'>jsPsych</a> JavaScript library. 
         jsPsych can be used to create behavioural experiments that can be run through a web browser. 
         My project provides an easy method of setting up a development environment, testing the plugin, and eventually will support exporting any required files in 
@@ -110,32 +62,32 @@ const IndexPage = () => {
         psygo will eventually be released as a Node JS-based CLI, and the source code is on my <a href='https://github.com/henry-burgess/psygo'>GitHub</a>.
       </p>
 
-      <h2 style={headingStyles}>
+      <h2 className={'headingStyles'}>
         coinspotapi
       </h2>
-      <p style={paragraphStyles}>
+      <p className={'paragraphStyles'}>
         This is an API wrapper written in Node.js for the Australian <a href='https://www.coinspot.com.au'>CoinSpot</a> cryptocurrency exchange.
         It will eventually provide access to all of the API functionality described in the CoinSpot API <a href='https://www.coinspot.com.au/api'>documentation</a> and is a work in progress.
         The source code is available publicly on <a href='https://github.com/henry-burgess/coinspotapi'>GitHub</a>, and the library can be installed by running <code>npm i @henryburgess/coinspotapi</code>.
       </p>
 
-      <h2 style={headingStyles}>
+      <h2 className={'headingStyles'}>
         Raspberry Pi guitar effects
       </h2>
-      <p style={paragraphStyles}>
+      <p className={'paragraphStyles'}>
         I used my Raspberry Pi to process audio input and apply various effects using the open-source Guitarix and JACK software. 
         I've put some code on <a href='https://github.com/henry-burgess/rpi-guitar-fx'>GitHub</a> that allows you to use two push-buttons to cycle the active Guitarix preset. 
-        You can read the entire process in the article I wrote on <a style={linkStyles} href='https://henryjburg.medium.com/guitar-effects-using-a-raspberry-pi-b24d39489a89'>Medium</a>.
+        You can read the entire process in the article I wrote on <a className={'linkStyles'} href='https://henryjburg.medium.com/guitar-effects-using-a-raspberry-pi-b24d39489a89'>Medium</a>.
       </p>
 
-      <h1 style={headingStyles}>
+      <h1 className={'headingStyles'}>
         Contact
       </h1>
-      <ul style={listStyles}>
+      <ul className={'listStyles'}>
         {links.map(link => (
-          <li style={listItemStyles}>
+          <li className={'listItemStyles'}>
             <a
-              style={linkStyles}
+              className={'linkStyles'}
               href={`${link.url}`}
             >
               {link.text}
